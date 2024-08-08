a18a35424552b69f3cdb85bc38d692957a20b230
amirhossein-sabouri <sabouriamirhossein9@gmail.com>
2024-08-08 14:29:35 +0330

    designing of web site view completed

--- assets/js/user.js
+++ assets/js/user.js
@@ -2,3 +2,36 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
   });
+
+  const addUserButton = document.getElementById("add_user_button");
+  addUserButton.addEventListener('click', () => {
+    document.querySelector('.add_user_modal').classList.add('show'); // Use . for class
+    document.querySelector('.add_user_modal_back').classList.remove('dis-none'); // Correct class name
+  });
+
+  document.querySelector('.add_user_modal_back').addEventListener('click',()=>{
+    document.querySelector('.add_user_modal').classList.remove('show')
+    document.querySelector('.add_user_modal_back').classList.add('dis-none')
+  })
+
+
+  const ctaElement = document.querySelector('.user_list_section div');
+
+
+  if(ctaElement.hasAttribute("target")){
+      console.log(ctaElement.getAttribute("target"));
+      
+  }else{
+    ctaElement.setAttribute("target","_blank");
+  }
+
+  console.log(ctaElement.attributes);
+  
+  
+
+  
+  
+
+
+
+  
